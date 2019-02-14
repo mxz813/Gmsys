7fc26a8a573fa89b5bdf34805fdb55055cf14a9a
mxz813 <178246180@qq.com>
2019-02-15 00:56:23 +0800

    add user login

--- Gmsys/urls.py
+++ Gmsys/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+from users.views import LoginView
 import xadmin
 
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
     path('', TemplateView.as_view(template_name= "index.html"),name = "index"),
+    path('login/', LoginView.as_view(),name = "login"),
 ]
---
+++ apps/users/forms.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+from django import forms
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True)
